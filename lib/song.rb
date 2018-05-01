65931b9d22dca369d0b6586cdc66368efa1c1734
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-05-01 00:28:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
 
   def self.find_or_create_by_name(song_title)
     song = nil
-    if self.find_by_name == song_title
+    if self.find_by_name 
       song = self.find_by_name
     else
       song = self.create_by_name
